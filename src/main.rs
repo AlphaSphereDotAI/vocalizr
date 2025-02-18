54575d17a3e2da2bba3a6268c306d961c00896a0
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-02-19 01:35:48 +0200

    feat: add serde dependency and implement audio generation endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
-mod model;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use model::generate;
+use actix_web::{get, http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+use sherpa_rs::tts::{KokoroTts, KokoroTtsConfig};
+use sherpa_rs::write_audio_file;
+
+#[derive(Deserialize)]
+struct GenerateParams {
+    text: String,
+    speaker_id: i32,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[get("/generate")]
+async fn generate(params: web::Query<GenerateParams>) -> impl Responder {
+    let config: KokoroTtsConfig = KokoroTtsConfig {
+        model: "./kokoro-en-v0_19/model.onnx".to_string(),
+        voices: "./kokoro-en-v0_19/voices.bin".into(),
+        tokens: "./kokoro-en-v0_19/tokens.txt".into(),
+        data_dir: "./kokoro-en-v0_19/espeak-ng-data".into(),
+        length_scale: 1.0,
+        ..Default::default()
+    };
+    let mut tts = KokoroTts::new(config);
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    // 0->af, 1->af_bella, 2->af_nicole, 3->af_sarah, 4->af_sky, 5->am_adam
+    // 6->am_michael, 7->bf_emma, 8->bf_isabella, 9->bm_george, 10->bm_lewis
+
+    let audio = tts.create(&params.text, params.speaker_id, 1.0).unwrap();
+
+    write_audio_file("audio.wav", &audio.samples, audio.sample_rate);
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body("Created audio.wav")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("0.0.0.0", 8001))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(generate))
+        .bind(("0.0.0.0", 8001))?
+        .run()
+        .await
 }
-
-// fn main() {
-//     generate();
-// }
