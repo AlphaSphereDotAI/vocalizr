ce1651b4dce241364503ca90e4337de47f6a03a4
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-02-19 19:20:49 +0200

    chore: remove deprecated FastAPI application and update server port in main.rs

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ async fn generate(
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    log::info!("starting HTTP server at http://localhost:8080");
+    log::info!("starting HTTP server at http://localhost:8001");
     HttpServer::new(|| {
         App::new()
             .service(hello)
